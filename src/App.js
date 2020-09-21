f9244b2ae616c324287b9aa27ca7d4097773f60a
omaradbelkader <omaradbelkader25@gmail.com>
2020-09-22 01:20:27 +0200

    react lazy load

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import './App.sass';
 import { Achivements, BreadCrumb, Details, Footer, Header, Navbar , PressReleases, Programs, Slider } from './components'
 import {
@@ -7,13 +7,14 @@ import {
   Route,
   Link
 } from "react-router-dom";
-const  NewsListing =  ()=> import('./views/NewsListing');
-const NewsDetails =  ()=>import('./views/NewsDetails');
-const HomePage = ()=> import('./views/HomePage')
-
+const  NewsListing =  React.lazy(()=> import('./views/NewsListing'));
+const NewsDetails =  React.lazy(()=>import('./views/NewsDetails'));
+const HomePage = React.lazy(()=> import('./views/HomePage'))
 function App() {
   return (
     <div className="App">
+      <Suspense fallback={<div>faild to load app</div>}>
+
       <Router basename={'link-dev-workshop'}>
 
        <Header/>
@@ -37,6 +38,7 @@ function App() {
         </Switch>
        <Footer/>
       </Router>
+      </Suspense>
     </div>
   );
 }
