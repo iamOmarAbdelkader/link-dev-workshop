9b07820b76af2f73ea66df0df6e41403c5e0739a
omaradbelkader <omaradbelkader25@gmail.com>
2020-09-21 17:03:15 +0200

    Updates

---
+++ precache-manifest.b687f687f1dfe8b72ae9ac7e232edc15.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f95fe6c1b43250db3be99cd1018a6a40",
+    "url": "/link-dev-workshop/index.html"
+  },
+  {
+    "revision": "3c892a4d0a49790d9371",
+    "url": "/link-dev-workshop/static/css/main.0146514b.chunk.css"
+  },
+  {
+    "revision": "c6e1c0899aa7249e296e",
+    "url": "/link-dev-workshop/static/js/2.232c865f.chunk.js"
+  },
+  {
+    "revision": "3098f83e5fa7a136279bcb0e71937139",
+    "url": "/link-dev-workshop/static/js/2.232c865f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c892a4d0a49790d9371",
+    "url": "/link-dev-workshop/static/js/main.8194635b.chunk.js"
+  },
+  {
+    "revision": "095b1ef42d5d873f02c7",
+    "url": "/link-dev-workshop/static/js/runtime-main.d39c41f3.js"
+  },
+  {
+    "revision": "5d7c31b284ddb01fc1cbae0edc9ec210",
+    "url": "/link-dev-workshop/static/media/Calibri Regular.5d7c31b2.ttf"
+  },
+  {
+    "revision": "69c36d2c33305af0a3a89d1d05851cc0",
+    "url": "/link-dev-workshop/static/media/OPTIMA.69c36d2c.woff"
+  },
+  {
+    "revision": "623c7118249e082fe87a78e08506cb4b",
+    "url": "/link-dev-workshop/static/media/Segoe UI.623c7118.ttf"
+  },
+  {
+    "revision": "bc5b64f9455e3fa81d392a32864d01bb",
+    "url": "/link-dev-workshop/static/media/achievements.bc5b64f9.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/link-dev-workshop/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/link-dev-workshop/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/link-dev-workshop/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/link-dev-workshop/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/link-dev-workshop/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "ec52e9adf3652d67685301e582896263",
+    "url": "/link-dev-workshop/static/media/linkdevlogo.ec52e9ad.png"
+  },
+  {
+    "revision": "6445843aa01613b661ed528bfb85a58d",
+    "url": "/link-dev-workshop/static/media/programs.6445843a.png"
+  },
+  {
+    "revision": "dc62daa6ff0ad8e6799bfd86c8f47f93",
+    "url": "/link-dev-workshop/static/media/recommends1.dc62daa6.png"
+  },
+  {
+    "revision": "7edd914f4c77e256b00feb24a04c1080",
+    "url": "/link-dev-workshop/static/media/recommends2.7edd914f.png"
+  },
+  {
+    "revision": "d8dcbba7e965683485cbf704e6620b3d",
+    "url": "/link-dev-workshop/static/media/recommends3.d8dcbba7.png"
+  },
+  {
+    "revision": "06daa92c422ffe5e4d9d9a9ef6c64a69",
+    "url": "/link-dev-workshop/static/media/sliderBg.06daa92c.png"
+  }
+]);
